a46af1d3c232c71b41fa31f314d04f26f5280299
Mia <mia.ma@here.com>
2018-01-16 13:08:55 -0600

    Idea Sample

--- mia/Array/1TwoSum.java
+++ mia/Array/1TwoSum.java
@@ -1,3 +1,13 @@
+/**
+* 给定一个数组（未排序），找到和为目标数的pair；
+
+用一个HashMap来记录: 每次访问一个element， 如果这个element没有在hashmap里出现，
+就存储 ［target - element, 当前元素的位置］
+这样如果下次这个元素出现了，那么说明map.get(element)就是 与element相加和为target的元素的位置
+注意 添加结果到res数组的顺序。
+
+空间 是 O(N) 时间是 O(N)
+*/
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] res = new int[2];
